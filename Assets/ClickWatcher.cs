560093346f546b32427fdb6ac46e10595d202710
Henry Peteet <hvpeteet@gmail.com>
2019-04-28 20:28:05 -0700

    Basic Growth works
    No children

--- Assets/ClickWatcher.cs
+++ Assets/ClickWatcher.cs
@@ -6,39 +6,58 @@ public class ClickWatcher : MonoBehaviour
 {
     public GameMutator mutator;
     public Material highlight_mat;
-    private List<MeshRenderer> to_revert = new List<MeshRenderer>();
-    private List<Material> revert_to = new List<Material>();
+    private List<Tile> highlighted = new List<Tile>();
+    private List<Material> highlighted_old_mats = new List<Material>();
+    private Tile selected;
+
+    void ClearHighlights()
+    {
+        // Clear old highlights.
+        for (int i = 0; i < highlighted.Count; i++)
+        {
+            highlighted[i].plant.gameObject.GetComponentInChildren<MeshRenderer>().material = highlighted_old_mats[i];
+        }
+        highlighted.Clear();
+        highlighted_old_mats.Clear();
+    }
 
     void Update()
     {
         Ray ray;
         RaycastHit hit;
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, Constants.TILES_LAYER_MASK))
+        if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hit, Mathf.Infinity, Constants.TILES_LAYER_MASK))
         {
-            if (Input.GetMouseButtonDown(0))
+            Tile clicked_tile = hit.collider.gameObject.GetComponent<Tile>();
+            Point coord = clicked_tile.hex_coords;
+            Debug.Log(string.Format("Clicked tile ({0}, {1})", coord.x, coord.y));
+
+            if (selected == null || !highlighted.Contains(clicked_tile))
             {
-                // Clear old highlights.
-                for (int i = 0; i < to_revert.Count; i++)
-                {
-                    to_revert[i].material = revert_to[i];
-                }
-                to_revert.Clear();
-                revert_to.Clear();
+                ClearHighlights();
 
                 // Get the surrounding tiles and highlight them.
-                Point coord = hit.collider.gameObject.GetComponent<Tile>().hex_coords;
-                Debug.Log(string.Format("Clicked tile ({0}, {1})", coord.x, coord.y));
                 List<Tile> to_highlight = mutator.GetNeighbors(hit.collider.gameObject.GetComponent<Tile>());
                 foreach (Tile t in to_highlight)
                 {
-                    MeshRenderer renderer = t.plant.gameObject.GetComponentInChildren<MeshRenderer>();
-                    to_revert.Add(renderer);
-                    revert_to.Add(renderer.material);
-                    renderer.material = highlight_mat;
+                    if (clicked_tile.plant.IsHabitable(t))
+                    {
+                        MeshRenderer renderer = t.plant.gameObject.GetComponentInChildren<MeshRenderer>();
+                        highlighted.Add(t);
+                        highlighted_old_mats.Add(renderer.material);
+                        renderer.material = highlight_mat;
+                    }
                 }
-                // Enable the UI for that tile.
+                selected = clicked_tile;
+            } else if (highlighted.Contains(clicked_tile))
+            {
+                ClearHighlights();
+                mutator.SetOrganism(selected.plant.GetOrganismType(), clicked_tile.GetComponent<Tile>().hex_coords);
+                selected = null;
             }
+
+                
+            // Enable the UI for that tile.
         }
     }
 }
